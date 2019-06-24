f4cd3b48143831b439f98bb1108f607ce8eb5aeb
PawelDemichowicz <pawel.demichowicz@interia.pl>
2019-06-24 23:07:26 +0200

    Task 7.7 ver2

--- kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
@@ -10,19 +10,20 @@ public class RpsRunner {
     private int quantityOfWins;
     private int comWins;
     private int gamerWins;
+    private final int ROCK = 1;
+    private final int PAPER = 2;
+    private final int SCISSORS = 3;
 
-    public int startGame() {
+    private void startGame() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give your name: ");
         this.name = scanner.nextLine();
 
         System.out.println("Give the number of wins: ");
         this.quantityOfWins = scanner.nextInt();
-
-        return quantityOfWins;
     }
 
-    public void gameSupport() {
+    private void gameSupport() {
         System.out.println("Buttons:");
         System.out.println("Button \"1\": Rock");
         System.out.println("Button \"2\": Paper");
@@ -31,7 +32,7 @@ public class RpsRunner {
         System.out.println("Button \"n\": New Game");
     }
 
-    public void controller() {
+    private void controller() {
         Scanner scanner = new Scanner(System.in);
         System.out.println(name + " make a move.");
         int move = scanner.nextInt();
@@ -40,47 +41,45 @@ public class RpsRunner {
         int computer = generator.nextInt(3) + 1;
 
         if (move != computer) {
-            if (move == 1 && computer == 2) {
+            if (move == ROCK && computer == PAPER) {
                 System.out.println("Rock vs Paper. Round for computer.");
                 this.comWins++;
                 System.out.println(gamerWins + ":" + comWins);
-            } else if (move == 1 && computer == 3) {
+            } else if (move == ROCK && computer == SCISSORS) {
                 System.out.println("Rock vs Scissors. Round for " + name + ".");
                 this.gamerWins++;
                 System.out.println(gamerWins + ":" + comWins);
-            }
-            if (move == 2 && computer == 3) {
+            } else if (move == PAPER && computer == SCISSORS) {
                 System.out.println("Paper vs Scissors. Round for computer.");
                 this.comWins++;
                 System.out.println(gamerWins + ":" + comWins);
-            } else if (move == 2 && computer == 1) {
+            } else if (move == PAPER && computer == ROCK) {
                 System.out.println("Paper vs Rock. Round for " + name + ".");
                 this.gamerWins++;
                 System.out.println(gamerWins + ":" + comWins);
-            }
-            if (move == 3 && computer == 1) {
+            } else if (move == SCISSORS && computer == ROCK) {
                 System.out.println("Scissors vs Rock. Round for computer.");
                 this.comWins++;
                 System.out.println(gamerWins + ":" + comWins);
-            } else if (move == 3 && computer == 2) {
+            } else if (move == SCISSORS && computer == PAPER) {
                 System.out.println("Scissors vs Paper. Round for " + name + ".");
                 this.gamerWins++;
                 System.out.println(gamerWins + ":" + comWins);
             }
-        } else if (move == 1) {
+        } else if (move == ROCK) {
             System.out.println("Rock vs Rock. Draw");
             System.out.println(gamerWins + ":" + comWins);
-        } else if (move == 2) {
+        } else if (move == PAPER) {
             System.out.println("Paper vs Paper. Draw");
             System.out.println(gamerWins + ":" + comWins);
-        } else if (move == 3) {
+        } else if (move == SCISSORS) {
             System.out.println("Scissors vs Scissors. Draw");
             System.out.println(gamerWins + ":" + comWins);
         }
 
     }
 
-    public void endGame() {
+    private void endGame() {
 
         if (comWins == quantityOfWins || gamerWins == quantityOfWins) {
             end = true;
@@ -94,7 +93,7 @@ public class RpsRunner {
             System.out.println("If you want keep play, please press \"n\" button or you can finish for now, by press \"x\" ");
             Scanner scanner = new Scanner(System.in);
             String choice = scanner.nextLine();
-            if (choice.equals("n")) {
+            if ("n".equals(choice)) {
                 comWins = 0;
                 gamerWins = 0;
                 end = false;
