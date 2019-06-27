0285808e3567da4d62ddb6f84e9737fefcef4d5a
PawelDemichowicz <pawel.demichowicz@interia.pl>
2019-06-27 22:00:17 +0200

    Task 15.3

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/social/User.java
@@ -0,0 +1,23 @@
+package com.kodilla.patterns.strategy.social;
+
+public class User {
+
+    final private String userName;
+    protected SocialPublisher socialPublisher;
+
+    public User(String name) {
+        this.userName = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String sharePost(){
+        return socialPublisher.share();
+    }
+
+    public void changeSocialMedia(SocialPublisher socialPublisher){
+        this.socialPublisher = socialPublisher;
+    }
+}
---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/social/UserTestSuite.java
@@ -0,0 +1,48 @@
+package com.kodilla.patterns.strategy.social;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserTestSuite {
+
+    @Test
+    public void testDefaultSocialMedia(){
+
+        //Given
+        User jacob = new Millenials("Jacob");
+        User steven = new YGeneration("Steven");
+        User paul = new ZGeneration("Paul");
+
+        //When
+        String jacobMedia = jacob.sharePost();
+        System.out.println(jacobMedia);
+        String stevenMedia = steven.sharePost();
+        System.out.println(stevenMedia);
+        String paulMedia = paul.sharePost();
+        System.out.println(paulMedia);
+
+        //Then
+        Assert.assertEquals("[Facebook]Sharing post...",jacobMedia);
+        Assert.assertEquals("[Twitter] Sharing post...",stevenMedia);
+        Assert.assertEquals("[Snapchat] Sharing post...",paulMedia);
+
+    }
+
+    @Test
+    public void testChangeSocialMedia(){
+
+        //Given
+        User jacob = new Millenials("Jacob");
+
+        //When
+        String jacobMedia = jacob.sharePost();
+        System.out.println("Old social media: " + jacobMedia);
+        jacob.changeSocialMedia(new TwitterPublisher());
+        jacobMedia = jacob.sharePost();
+        System.out.println("New social media: " + jacobMedia);
+
+        //Then
+        Assert.assertEquals("[Twitter] Sharing post...", jacobMedia);
+
+    }
+}
