bcaab942b8553439d4769c647b4d05e052546b15
PawelDemichowicz <pawel.demichowicz@interia.pl>
2019-07-14 21:56:57 +0200

    Task 17.1 ver 1.0

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/dao/TaskDao.java
@@ -0,0 +1,10 @@
+package com.kodilla.hibernate.task.dao;
+
+import com.kodilla.hibernate.task.Task;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskDao extends CrudRepository<Task, Integer> {
+
+}
