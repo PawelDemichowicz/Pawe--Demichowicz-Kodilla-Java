25196437603f482ed3093e7fc1fcd413919763e0
PawelDemichowicz <pawel.demichowicz@interia.pl>
2019-10-16 09:45:53 +0200

    Task 24.4 - change snake_case to camelCase

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/TaskQueueTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/TaskQueueTestSuite.java
@@ -9,20 +9,20 @@ public class TaskQueueTestSuite {
     @Test
     public void testRegisterObserver(){
         //Given
-        TaskQueue smith_queue = new TaskQueue("John Smith");
-        TaskQueue escobar_queue = new TaskQueue("Ivone Escobar");
-        TaskQueue pinkam_queue = new TaskQueue("Jessie Pinkam");
+        TaskQueue smithQueue = new TaskQueue("John Smith");
+        TaskQueue escobarQueue = new TaskQueue("Ivone Escobar");
+        TaskQueue pinkamQueue = new TaskQueue("Jessie Pinkam");
         Mentor mentor1 = new Mentor("Rob Anderson");
         Mentor mentor2 = new Mentor("Ludovico Lancetto");
-        smith_queue.registerObserver(mentor1);
-        escobar_queue.registerObserver(mentor1);
-        pinkam_queue.registerObserver(mentor2);
+        smithQueue.registerObserver(mentor1);
+        escobarQueue.registerObserver(mentor1);
+        pinkamQueue.registerObserver(mentor2);
         //When
-        smith_queue.addTask("Facade pattern");
-        smith_queue.addTask("Decorator pattern");
-        escobar_queue.addTask("Aspect programming");
-        pinkam_queue.addTask("Observer pattern");
-        smith_queue.addTask("Adapter pattern");
+        smithQueue.addTask("Facade pattern");
+        smithQueue.addTask("Decorator pattern");
+        escobarQueue.addTask("Aspect programming");
+        pinkamQueue.addTask("Observer pattern");
+        smithQueue.addTask("Adapter pattern");
         //Then
         assertEquals(4,mentor1.getTasksCount());
         assertEquals(1,mentor2.getTasksCount());
@@ -31,38 +31,38 @@ public class TaskQueueTestSuite {
     @Test
     public void testRemoveObserver(){
         //Given
-        TaskQueue smith_queue = new TaskQueue("John Smith");
-        TaskQueue escobar_queue = new TaskQueue("Ivone Escobar");
-        TaskQueue pinkam_queue = new TaskQueue("Jessie Pinkam");
+        TaskQueue smithQueue = new TaskQueue("John Smith");
+        TaskQueue escobarQueue = new TaskQueue("Ivone Escobar");
+        TaskQueue pinkamQueue = new TaskQueue("Jessie Pinkam");
         Mentor mentor1 = new Mentor("Rob Anderson");
         Mentor mentor2 = new Mentor("Ludovico Lancetto");
-        smith_queue.registerObserver(mentor1);
-        escobar_queue.registerObserver(mentor1);
-        pinkam_queue.registerObserver(mentor2);
-        smith_queue.removeObserver(mentor1);
+        smithQueue.registerObserver(mentor1);
+        escobarQueue.registerObserver(mentor1);
+        pinkamQueue.registerObserver(mentor2);
+        smithQueue.removeObserver(mentor1);
         //When
-        smith_queue.addTask("Facade pattern");
-        smith_queue.addTask("Decorator pattern");
-        escobar_queue.addTask("Aspect programming");
-        pinkam_queue.addTask("Observer pattern");
-        smith_queue.addTask("Adapter pattern");
+        smithQueue.addTask("Facade pattern");
+        smithQueue.addTask("Decorator pattern");
+        escobarQueue.addTask("Aspect programming");
+        pinkamQueue.addTask("Observer pattern");
+        smithQueue.addTask("Adapter pattern");
 
         //Then
         assertEquals(1,mentor1.getTasksCount());
         assertEquals(1,mentor2.getTasksCount());
-        assertEquals(0,smith_queue.getObservers().size());
+        assertEquals(0,smithQueue.getObservers().size());
     }
 
     @Test
     public void testNotifyObserver(){
         //Given
-        TaskQueue smith_queue = new TaskQueue("John Smith");
-        Mentor mentor1 = new Mentor("Rob Anderson");
-        smith_queue.registerObserver(mentor1);
+        TaskQueue smithQueue = new TaskQueue("John Smith");
+        Mentor mentor = new Mentor("Rob Anderson");
+        smithQueue.registerObserver(mentor);
         //When
-        smith_queue.notifyObservers();
-        int result = mentor1.getTasksCount();
+        smithQueue.notifyObservers();
+        int result = mentor.getTasksCount();
         //Then
-        assertEquals(1,mentor1.getTasksCount());
+        assertEquals(1,mentor.getTasksCount());
     }
 }
